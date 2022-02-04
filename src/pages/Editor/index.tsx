ef7397399861adb3982105514c3a63033505d919
inikulin <inikulin@ibsys.ru>
2022-02-04 22:10:53 +0500

    fix some styles make default value for config fix tsconfig (set bathPath)

--- src/pages/Editor/index.tsx
+++ src/pages/Editor/index.tsx
@@ -1,16 +1,23 @@
 import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { ConfigContext } from 'src/contexts/config';
 import JsonEditor, { JsonEditorProps } from 'src/components/json-editor';
+import Button from 'src/components/common/button';
+import { ROUTE } from 'src/constans/routes';
 
 const EditorPage: React.FC = () => {
+  const history = useHistory();
   const [config, setConfig] = useContext(ConfigContext);
 
   return (
-    <JsonEditor
-      value={config || []}
-      onChange={setConfig as JsonEditorProps['onChange']}
-    />
+    <div>
+      <JsonEditor
+        value={config || []}
+        onChange={setConfig as JsonEditorProps['onChange']}
+      />
+      <Button label="Apply" onClick={() => history.push(ROUTE.FORM)} />
+    </div>
   );
 };
 
